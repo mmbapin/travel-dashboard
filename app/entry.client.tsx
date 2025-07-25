ec278e1d393836f53c84fbe5bb54325396ce1e11
mmbapin <bapinmm@gmail.com>
2025-07-25 12:59:10 +0600

    integrated sentry

---
+++ app/entry.client.tsx
@@ -0,0 +1,38 @@
+import * as Sentry from "@sentry/react-router";
+ import { startTransition, StrictMode } from "react";
+ import { hydrateRoot } from "react-dom/client";
+ import { HydratedRouter } from "react-router/dom";
+
+Sentry.init({
+ dsn: "https://ff6653739b46edcbc8f22abe253e528a@o4509689644318720.ingest.us.sentry.io/4509727537561600",
+ 
+ // Adds request headers and IP for users, for more info visit:
+ // https://docs.sentry.io/platforms/javascript/guides/react-router/configuration/options/#sendDefaultPii
+ sendDefaultPii: true,
+ integrations: [
+   Sentry.reactRouterTracingIntegration(),
+   Sentry.replayIntegration(),
+   Sentry.feedbackIntegration({
+     // Additional SDK configuration goes in here, for example:
+     colorScheme: "system",
+   }),
+ ],
+ // Enable logs to be sent to Sentry
+ _experiments: { enableLogs: true },
+ tracesSampleRate: 1.0, //  Capture 100% of the transactions
+ // Set `tracePropagationTargets` to declare which URL(s) should have trace propagation enabled
+ tracePropagationTargets: [/^\//, /^https:\/\/yourserver\.io\/api/],
+ // Capture Replay for 10% of all sessions,
+ // plus 100% of sessions with an error
+ replaysSessionSampleRate: 0.1,
+ replaysOnErrorSampleRate: 1.0,
+});
+
+startTransition(() => {
+  hydrateRoot(
+    document,
+    <StrictMode>
+      <HydratedRouter />
+    </StrictMode>
+  );
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,31 @@
 import { reactRouter } from "@react-router/dev/vite";
+import { sentryReactRouter, type SentryReactRouterBuildOptions } from "@sentry/react-router";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+const sentryConfig: SentryReactRouterBuildOptions = {
+  org: "js-mastery-0u",
+  project: "javascript-react",
+  // An auth token is required for uploading source maps;
+  // store it in an environment variable to keep it secure.
+  authToken:"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",
+  // ...
+};
+
+// export default defineConfig({
+//   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+//   ssr: {
+//     noExternal: [/@syncfusion/]
+//   }
+// });
+
+export default defineConfig(config => {
+  return {
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), sentryReactRouter(sentryConfig, config)],
+  sentryConfig,
   ssr: {
     noExternal: [/@syncfusion/]
   }
+  };
 });
